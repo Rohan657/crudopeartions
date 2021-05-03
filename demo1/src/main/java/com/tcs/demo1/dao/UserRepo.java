d309b4d9b7272bbc56d7ebcf0bc56d2b5a8013fd
Rohan <kritika@DESKTOP-5TGL241>
2021-05-03 14:03:45 +0530

    crud opeartions with h2

--- demo1/src/main/java/com/tcs/demo1/dao/UserRepo.java
+++ demo1/src/main/java/com/tcs/demo1/dao/UserRepo.java
@@ -1,15 +1,11 @@
 package com.tcs.demo1.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 import com.tcs.demo1.Entity.User;
 
 public interface UserRepo extends JpaRepository<User, Integer>
 {
-	@Modifying
-    @Query("Update User u Set u.address=:s Where u.id=:id")
-    public void update_address(@Param("s") String s,@Param("id") Integer id);
+	
 }
